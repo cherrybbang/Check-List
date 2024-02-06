79392c10a203ddc6682f04c9384159f03204c702
cherrybbang <146607445+cherrybbang@users.noreply.github.com>
2024-02-06 11:11:56 +0900

    뷰 배포 전

--- check_list.js
+++ check_list.js
@@ -1,6 +1,12 @@
 const click = document.querySelector('#click');
-const opt = document.querySelector('.ccc');
+const change = document.querySelector('.delete');
+
 
 click.onclick = function(){
-    
+  change.innerHTML = '<del>하 존나 인생이란...</del>'
 }
+
+
+
+// 클릭시 <del>을 붙인다...
+// css에 따로 지정해주어도 된다. .click {backgroudn : #000} 나중에 복합 클래스로 달아주면 되니까~ 
