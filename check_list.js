61ce35969093626b81cb837a1990434d139c29e3
cherrybbang <146607445+cherrybbang@users.noreply.github.com>
2024-02-07 21:20:49 +0900

    js, label 태그 수정

--- check_list.js
+++ check_list.js
@@ -1,5 +1,5 @@
 const click = document.querySelector('#click');
-const change = document.querySelector('.delete');
+// const change = document.querySelector('.delete');
 // change.innerHTML = `<del>${}</del>`;
 
 
@@ -8,6 +8,20 @@ const change = document.querySelector('.delete');
 //   change.innerHTML = '<del>하 존나 인생이란...</del>'
 // }
 
-click.addEventListener('click', function(){
-  click.classList.toggle('delete');
-})
+// click.addEventListener('click', function(){
+//   click.classList.toggle('delete');
+// })
+
+// function clickCheck(){
+//   if(click.classList.contains('delete')){
+//     click.classList.remove('delete')
+//   } else {
+//     click.classList.add('delete')
+//   }
+// }
+
+// click.addEventListener('click', clickCheck);
+
+click.onclick = function(){
+  click.classList.toggle('delete')
+}
